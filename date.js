1f84adf0e4781e08c5c956ebcb530f1e1e0615eb
winsrewu <78122384+winsrewu@users.noreply.github.com>
2022-09-30 20:14:10 +0800

    0.0.3 A lot! about & css

--- date.js
+++ date.js
@@ -1,61 +1,56 @@
-function isLeap(year){
+function isLeap(year) {
 	//判断是否闰年
-	if(year % 400 == 0){
+	if (year % 400 == 0) {
 		return true;
 	}
-	if(year % 100 != 0 && year % 4 == 0){
+	if (year % 100 != 0 && year % 4 == 0) {
 		return true;
-	}
-	else{
+	} else {
 		return false;
 	}
 }
 
-function isData(x){
+function isData(x) {
 	x = +x;
-	if(x != x){
+	if (x != x) {
 		return "Not A Number!"
 	}
-	if(x <= 0){
+	if (x <= 0) {
 		return "Too Low!"
 	}
-	if(!(Math.round(x) === x)){
+	if (!(Math.round(x) === x)) {
 		return "Natural Number Please"
-	}
-	else{
+	} else {
 		return "ok"
 	}
 }
 
-function input(){
+function input() {
 	document.getElementById("output").innerHTML = "";
 
 	var n_day = prompt('输入当前天数');
 
-	if(isData(n_day) != "ok"){
+	if (isData(n_day) != "ok") {
 		window.alert(isData(n_day));
 		document.getElementById("output").innerHTML = "";
-	}
-	else{
+	} else {
 		//获得年
 		var n_year = 0;
 		var day = 0;
-		do{
+		do {
 			n_year += 1;
-			if(isLeap(n_year)){
+			if (isLeap(n_year)) {
 				day += 366;
-			}
-			else{
+			} else {
 				day += 365;
 			}
 		}
-		while(day < n_day)
+		while (day < n_day)
 
 		//获得今年的第几日
-		if(isLeap(n_year)){
+		if (isLeap(n_year)) {
 			day = 366 - (day - n_day);
-		}
-		else{
+		} else {
 			day = 365 - (day - n_day);
 		}
 
@@ -68,18 +63,17 @@ function input(){
 		var dayB = 0;
 
 		//获得月份
-		if(!(isLeap(n_year))){
-			do{
+		if (!(isLeap(n_year))) {
+			do {
 				month += 1;
 				dayB += daysinMonth[month - 1];
-			}while(dayB < day);
+			} while (dayB < day);
 			day = daysinMonth[month - 1] - (dayB - day);
-		}
-		else{
-			do{
+		} else {
+			do {
 				month += 1;
 				dayB += daysinLeap[month - 1];
-			}while(dayB < day);
+			} while (dayB < day);
 			day = daysinLeap[month - 1] - (dayB - day);
 		}
 
@@ -88,24 +82,25 @@ function input(){
 
 		var timeH = document.getElementById("timeHour").value;
 		var timeM = document.getElementById("timeMinute").value;
-		
+
 		var obj = document.getElementsByName("timeOff");
-		if(!obj[0].checked){
-			var InGameTime = "IN GAME TIME: " + n_year + "年" + month + "月" + day + "日 " + weekname[(n_day - 1) % 7] + " " + timeH + ":" + timeM;
-		}
-		else{
+		if (!obj[0].checked) {
+			var InGameTime = "IN GAME TIME: " + n_year + "年" + month + "月" + day + "日 " + weekname[(n_day - 1) % 7] +
+				" " + timeH + ":" + timeM;
+		} else {
 			var InGameTime = "IN GAME TIME: " + n_year + "年" + month + "月" + day + "日 " + weekname[(n_day - 1) % 7];
 		}
 
-		
+
 		var now = new Date();
-		if(!obj[1].checked){
-			var RealityTime = "REALITY TIME: " + now.getFullYear() + "年" + (now.getMonth() + 1) + "月" + now.getDate() + "日 " + weekname[now.getDay() - 1] + " " + now.getHours() + ":" + now.getMinutes();
+		if (!obj[1].checked) {
+			var RealityTime = "REALITY TIME: " + now.getFullYear() + "年" + (now.getMonth() + 1) + "月" + now.getDate() +
+				"日 " + weekname[now.getDay() - 1] + " " + now.getHours() + ":" + now.getMinutes();
+		} else {
+			var RealityTime = "REALITY TIME: " + now.getFullYear() + "年" + (now.getMonth() + 1) + "月" + now.getDate() +
+				"日 " + weekname[now.getDay() - 1];
 		}
-		else{
-			var RealityTime = "REALITY TIME: " + now.getFullYear() + "年" + (now.getMonth() + 1) + "月" + now.getDate() + "日 " + weekname[now.getDay() - 1];
-		}
-		
+
 
 		document.getElementById("output").innerHTML = InGameTime + " | " + RealityTime;
 	}
