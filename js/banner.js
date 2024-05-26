70f14e028b43c0e8d66139621ebf209ca9ae2026
winsrewu <78122384+winsrewu@users.noreply.github.com>
2024-05-26 15:12:09 +0800

    feat: support banner text multisentence

--- js/banner.js
+++ js/banner.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
+	let textList = localStorage.getItem("bannerText");
+	if (textList) {
+		textList = JSON.parse(textList);
+		if (textList.length > 2) {
+			localStorage.setItem("bannerText", JSON.stringify(textList.splice(2)));
+		} else {
+			localStorage.removeItem("bannerText");
+		}
+		$(".bannerTA").text(textList[0]);
+		$(".bannerTB").text(textList[1]);
+		return;
+	}
 	$.ajax({
 		type: 'GET',
 		url: 'https://api.jawbts.org/text',
 		data: {},
 		dataType: 'JSON',
 		success: function(data) {
+			if (data.data.context.length > 0) {
+				let textList = data.data.context;
+				console.log(textList);
+				localStorage.setItem("bannerText", JSON.stringify(textList.splice(2)));
+				$(".bannerTA").text(textList[0]);
+				$(".bannerTB").text(textList[1]);
+				return;
+			}
 			$(".bannerTA").text(data.data.a);
 			$(".bannerTB").text(data.data.b);
 		},
