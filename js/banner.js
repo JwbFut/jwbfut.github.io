cdbbfb42e7de0a055a8b2de6514c3ebb4eebd6b9
WisW <78122384+winsrewu@users.noreply.github.com>
2025-12-14 14:02:50 +0800

    feat: enhanced banner text load

--- js/banner.js
+++ js/banner.js
@@ -1,32 +1,59 @@
-$(document).ready(function() {
-	let textList = localStorage.getItem("bannerText");
-	if (textList) {
+localStorage.setItem("bannerTextSet", false);
+
+let textList = localStorage.getItem("bannerText");
+if (textList) {
+	try {
 		textList = JSON.parse(textList);
-		if (textList.length > 2) {
-			localStorage.setItem("bannerText", JSON.stringify(textList.splice(2)));
-		} else {
-			localStorage.removeItem("bannerText");
-		}
-		$(".bannerTA").text(textList[0]);
-		$(".bannerTB").text(textList[1]);
-		return;
+	} catch (e) {
+		textList = null;
 	}
+}
+
+if (textList && textList.length > 2) {
+	localStorage.setItem("bannerText", JSON.stringify(textList.splice(2)));
+} else {
+	localStorage.removeItem("bannerText");
+}
+
+if (textList) {
+	localStorage.setItem("bannerTextA", textList[0]);
+	localStorage.setItem("bannerTextB", textList[1]);
+} else {
 	$.ajax({
 		type: 'GET',
 		url: 'https://api.jawbts.org/text',
 		data: {},
 		dataType: 'JSON',
-		success: function(data) {
-			if (data.data.context.length > 0) {
+		success: function (data) {
+			if (data.data?.context?.length > 0) {
 				let textList = data.data.context;
-				console.log(textList);
 				localStorage.setItem("bannerText", JSON.stringify(textList.splice(2)));
-				$(".bannerTA").text(textList[0]);
-				$(".bannerTB").text(textList[1]);
+				localStorage.setItem("bannerTextA", textList[0]);
+				localStorage.setItem("bannerTextB", textList[1]);
+
+				setBanner();
+
 				return;
 			}
-			$(".bannerTA").text(data.data.a);
-			$(".bannerTB").text(data.data.b);
+			localStorage.setItem("bannerTextA", data.data?.a);
+			localStorage.setItem("bannerTextB", data.data?.b);
+
+			setBanner();
 		},
 	})
+}
+
+function setBanner() {
+	if (localStorage.getItem("bannerTextA") && localStorage.getItem("bannerTextB") && localStorage.getItem("bannerTextSet") !== "true") {
+		$(".bannerTA").text(localStorage.getItem("bannerTextA"));
+		$(".bannerTB").text(localStorage.getItem("bannerTextB"));
+
+		localStorage.removeItem("bannerTextA");
+		localStorage.removeItem("bannerTextB");
+		localStorage.setItem("bannerTextSet", true);
+	}
+}
+
+$(document).ready(function () {
+	setBanner();
 });
